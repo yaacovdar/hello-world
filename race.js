6069f86c3fdfe0e99835de7a69d3aa63955592c1
yaacovdar <yaacov.dar@gmail.com>
2026-03-14 12:34:08 +0200

    Create race.js

---
+++ race.js
@@ -0,0 +1,167 @@
+window.EXTERNAL_RIDDLES = [
+    { 
+        type: 'code', 
+        answer: '55555', 
+        text: 'המילה הששית בעמ\' 43 של איזדורה מון' 
+    },
+    { 
+        type: 'text', 
+        answer: ['אתה'], 
+        text: 'איזו חברה המקרר במטבח?' 
+    },
+    { 
+        type: 'text', 
+        answer: ['LIBBHERR', 'Libbherr'], 
+        text: 'מעל מקרר היין יש בקבוקים בכל מני צבעים, אם מתעלמים מהבקבוקים השקופים מה הצבעים של הבקבוקים?' 
+    },
+    { 
+        type: 'sequence', 
+        answer: ['blue', 'yellow', 'green', 'red', 'blue'], 
+        text: 'שש ספרות אחרונות בברקוד של חלב' 
+    },
+    { 
+        type: 'code', 
+        answer: '131074', 
+        text: 'מה כתוב על המדיח בגג' 
+    },
+    { 
+        type: 'text', 
+        answer: ['Electra', 'אלקטרה', 'ELECTRA'], 
+        text: 'המילה הלפני אחרונה בספר של דורה' 
+    },
+    { 
+        type: 'text', 
+        answer: ['סבתא'], 
+        text: 'איזו חברה הרמקול הורוד?' 
+    },
+    { 
+        type: 'text', 
+        answer: ['JBL', 'jbl'], 
+        text: 'הטלפון של חבצלת מרקט' 
+    },
+    { 
+        type: 'text', 
+        answer: ['09-8663311', '098663311', '8663311'], 
+        text: 'הצבעים בלוגו של גוגל לפי הסדר' 
+    },
+    { 
+        type: 'code', 
+        answer: '098663311', 
+        text: 'הצבעים בלוגו של גוגל לפי הסדר' 
+    },
+    { 
+        type: 'sequence', 
+        answer: ['blue', 'red', 'yellow', 'blue', 'green', 'red'], 
+        text: 'כמה עגבניות יש? כתוב את המספר פעמיים' 
+    },
+    { 
+        type: 'code', 
+        answer: '3939', 
+        text: 'הטלפון של גל המדען' 
+    },
+    { 
+        type: 'text', 
+        answer: ['052-4530288', '0524530288', '4530288'], 
+        text: 'מה כתוב על הכפתור הכחול של השלט של המזגן?' 
+    },
+    { 
+        type: 'code', 
+        answer: '0524530288', 
+        text: 'מה כתוב על הכפתור הכחול של השלט של המזגן?' 
+    },    
+    { 
+        type: 'text', 
+        answer: ['COOL', 'cool'], 
+        text: 'מה כתוב על הכפתור האדום בשלט של המזגן?' 
+    },
+    { 
+        type: 'text', 
+        answer: ['HEAT', 'heat'], 
+        text: 'איזו חברה התנור במטבח?' 
+    },
+    { 
+        type: 'text', 
+        answer: ['GORENJE', 'Gorenje', 'גורנייה'], 
+        text: 'מה כתוב על הטרנינג של אלון?' 
+    },
+    { 
+        type: 'text', 
+        answer: ['Hot wheels', 'HotWheels', 'hot wheels'], 
+        text: 'רגליים של חד קרן: שמאל אחורית, ימין אחורית, שמאל קדמית, ימין קדמית' 
+    },
+    { 
+        type: 'sequence', 
+        answer: ['red', 'green', 'blue', 'yellow'], 
+        text: 'דאנאקוד של הנסיך הקטן' 
+    },
+    { 
+        type: 'text', 
+        answer: ['3211262'. '32-11262'], 
+        text: 'הצבעים בצעצוע רחוב סומסום של גאיה?' 
+    },   
+    { 
+        type: 'code', 
+        answer: '3211262', 
+        text: 'הצבעים בצעצוע רחוב סומסום של גאיה?' 
+    },
+    { 
+        type: 'sequence', 
+        answer: ['green', 'yellow', 'red', 'green', 'yellow'], 
+        text: 'צריך ללחוץ על הכפתור בצבע של הכבל שמחובר לטלוויזיה כמספר הכסאות שיש ליד השולחן בסלון' 
+    },
+    { 
+        type: 'sequence', 
+        answer: ['blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue'], 
+        text: 'בגג ליד הקומקום יש סביבון "נס גדול היה פה היה פה נס גדול"' 
+    },
+    { 
+        type: 'sequence', 
+        answer: ['red', 'blue', 'red', 'green', 'red', 'green', 'red', 'blue'], 
+        text: 'מה הצבע של הפרחים ליד חדר השינה של סבא וסבתא? חובה לענות באנגלית!' 
+    },
+    { 
+        type: 'text', 
+        answer: ['Purple', 'purple'], 
+        text: 'מה השם של משחת השיניים של אלון?' 
+    },
+    { 
+        type: 'text', 
+        answer: ['Orbitol', 'Orbitol kids', 'אורביטול'], 
+        text: 'מה כתוב על המקרר בגג?' 
+    },
+    { 
+        type: 'text', 
+        answer: ['King', 'king', 'קינג'], 
+        text: 'הקוד שפותח את החידה הבאה הוא כל המספרים האי זוגיים בסדר עולה' 
+    },
+    { 
+        type: 'code', 
+        answer: '13579', 
+        text: 'התשובה לחידה הבאה בתוך נרתיק המשקפיים של אורן' 
+    },
+    { 
+        type: 'text', 
+        answer: ['בולבוס', 'בולבוס!'], 
+        text: 'הקליקו על הצבעים בשורה הראשונה בקיר הצפוני של המטבח מימין לשמאל ומלמעלה למטה' 
+    },
+    { 
+        type: 'sequence', 
+        answer: ['green', 'red', 'yellow', 'red'], 
+        text: 'מה הצבע של הכסא הנמוך בממד? חובה לענות באנגלית' 
+    },
+    { 
+        type: 'text', 
+        answer: ['Yellow', 'yellow'], 
+        text: 'לפרפרים יש חידת קלפים' 
+    },
+    { 
+        type: 'code', 
+        answer: '6478', 
+        text: 'כמה קלוריות יש ב100 גרם עוגיות ריבה מרולדין?' 
+    },
+    { 
+        type: 'code', 
+        answer: '459', 
+        text: 'מזל טוב! סיימתם את המירוץ! הפרס הוא העוגיות שאתם מחזיקים ביד! (אבל לקינוח בסדר?)' 
+    }
+];
