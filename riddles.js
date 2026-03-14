2cac68d890486a0cbbd16c109551fc4c9921d6c9
yaacovdar <yaacov.dar@gmail.com>
2026-03-14 11:11:20 +0200

    Create riddles.js

---
+++ riddles.js
@@ -0,0 +1,25 @@
+/**
+ * ==========================================
+ * EXTERNAL RIDDLE DATA
+ * Save this file as 'riddles.js' and point
+ * the URL to it: treasure_hunt.html?data=riddles.js
+ * ==========================================
+ */
+
+window.EXTERNAL_RIDDLES = [
+    { 
+        type: 'code', 
+        answer: '9999', 
+        text: 'External code works! Go to the balcony.' 
+    },
+    { 
+        type: 'sequence', 
+        answer: ['blue', 'yellow', 'blue'], 
+        text: 'External pattern matched. Look in the cookie jar!' 
+    },
+    { 
+        type: 'text', 
+        answer: ["treasure", "gold"], 
+        text: 'External word found. The prize is in the garage!' 
+    }
+];
